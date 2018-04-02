a1ff8988eb3c480d7ccf423ddfa6cb30a93d3786
forafish <fora.luo@gmail.com>
2018-04-02 13:02:55 +0800

    Fix splitOrArray for an object

--- src/string.js
+++ src/string.js
@@ -1,13 +1,23 @@
 import R from 'ramda';
 
 /**
- * split path to get head or tail
+ * Split path to get head or tail
  */
 export const splitHead = R.compose(R.head, R.split('.'));
 export const splitTail = R.compose(R.join('.'), R.tail, R.split('.'));
 
+/**
+ * Always get an array of a possible input.
+ * if it is a string, split it;
+ * if it is already an array, return itself;
+ * if it is an object, wrap it in an array.
+ */
 export const splitOrArray = R.ifElse(
   R.is(Array),
   R.identity,
-  R.split(',')
+  R.ifElse(
+    R.is(Object),
+    o => [o],
+    R.split(',')
+  )
 );

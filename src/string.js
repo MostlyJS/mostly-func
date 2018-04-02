3dca168a68d4cd98252754c4b34f4136ef1434a6
forafish <fora.luo@gmail.com>
2018-04-02 11:30:41 +0800

    add splitOrArray

--- src/string.js
+++ src/string.js
@@ -5,3 +5,9 @@ import R from 'ramda';
  */
 export const splitHead = R.compose(R.head, R.split('.'));
 export const splitTail = R.compose(R.join('.'), R.tail, R.split('.'));
+
+export const splitOrArray = R.ifElse(
+  R.is(Array),
+  R.identity,
+  R.split(',')
+);

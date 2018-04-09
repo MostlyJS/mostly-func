e6a8e84307b565ef9fd035677eddcfffd0d6d3b4
forafish <fora.luo@gmail.com>
2018-04-10 02:52:47 +0800

    add isObjectId

--- src/type.js
+++ src/type.js
@@ -30,3 +30,16 @@ export isFunction = R.anyPass(
   val => Object.prototype.toString.call(val) === '[object AsyncFunction]',
   val => Object.prototype.toString.call(val) === '[object GeneratorFunction]'
 );
+
+/**
+ * is hexadecimal
+ */
+export const isHex = R.test(/^[0-9A-F]+$/i);
+
+/**
+ * is mongo objectId
+ */
+export const isObjectId = (str) => {
+  return isHex(str) && str.length === 24;
+};
+

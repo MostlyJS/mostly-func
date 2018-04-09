8c9193afbbacd8a74b299e1fcd538ca2118cf65c
forafish <fora.luo@gmail.com>
2018-04-10 04:12:54 +0800

    add idPropEq

--- src/relation.js
+++ src/relation.js
@@ -1,15 +1,22 @@
 import R from 'ramda';
 
 /**
- * some convenient complement
+ * Some convenient complement
  */
 export const isNotEquals = R.both(R.complement(R.isNil), R.complement(R.equals));
 
 /**
- * is mongo objectId equals with string
+ * Is mongo objectId/string equals with another
  */
 export const idEquals = R.curry((a, b) => {
   if (a && typeof a.equals === 'function') return a.equals(b);
   if (b && typeof b.equals === 'function') return b.equals(a);
   return R.equals(a, b);
 });
+
+/**
+ * Returns true if prop equals as mongo objectId/string
+ */
+export const idPropEq = R.curry((name, val, obj) =>
+  R.propSatisfies(idEquals(val), name, obj)
+);

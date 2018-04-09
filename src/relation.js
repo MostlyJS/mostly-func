f38eb260f2f9cecac7e48448b0d3ed073e21ac67
forafish <fora.luo@gmail.com>
2018-04-10 03:18:58 +0800

    add idEquals

--- src/relation.js
+++ src/relation.js
@@ -4,3 +4,10 @@ import R from 'ramda';
  * some convenient complement
  */
 export const isNotEquals = R.both(R.complement(R.isNil), R.complement(R.equals));
+
+/**
+ * is mongo objectId equals with string
+ */
+export const idEquals = R.curry((a, b) =>
+  a && typeof a.equals === 'function'? a.equals(b) : R.equals(a, b)
+);

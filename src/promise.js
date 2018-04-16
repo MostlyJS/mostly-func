ad628d94d6a25e7e0839fda5b5f353a241299838
forafish <fora.luo@gmail.com>
2018-04-16 15:41:46 +0800

    add retryOrFail

--- src/promise.js
+++ src/promise.js
@@ -18,3 +18,56 @@ export const allP = promises => Promise.all(promises);
 // :: Number -> Promise.Resolved ()
 export const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
+
+/**
+ * Utility function to retry getting data from somewhere N times
+ * with some delays. It works with Promisified `retryFn`.
+ *
+ * Arguments:
+ * - `delays`  - a list of delays (numbers) between each attempt in ms.
+ *               If all attempts has left it will return Rejected Promise
+ *               with latest data, passed through `errFn`.
+ * - `stopFn`  - predicate function to stop retrying if Promise rejected
+ *               with an Error, that should be returned. If in some case
+ *               no such conditions, just return `false` on each call.
+ * - `errFn`   - function, that could transform catched error.
+ * - `retryFn` - function to retry
+ *
+ * E.G.
+ * ```
+ * // Let's imagine that `fetchSomething` fetches some data and returns
+ * // Resolved Promise on 200OK status, in all other cases returns Rejected
+ * fetchSomething()
+ *   .catch(
+ *     retryOrFail(
+ *       [500, 1000, 2000], // retry after 500ms, then 1000ms, then 2000ms
+ *       errRes => (errRes.status && errRes.status === 404), // stop if server returned 404
+ *       R.identity, // do not transform errored server response
+ *       fetchSomething
+ *     )
+ *   );
+ * ```
+ */
+// :: [Number] -> (b -> Boolean) -> (b -> Error) -> (() -> Promise a b) -> (b -> Promise a b)
+export const retryOrFail = R.curry((delays, stopFn, errFn, retryFn) => {
+  const maxRetries = delays.length;
+  let retryCounter = 0;
+
+  const run = data =>
+    new Promise((resolve, reject) => {
+      if (stopFn(data) || retryCounter === maxRetries) {
+        reject(errFn(data));
+        return;
+      }
+
+      setTimeout(() => {
+        retryCounter += 1;
+        retryFn()
+          .catch(run)
+          .then(resolve)
+          .catch(reject);
+      }, delays[retryCounter]);
+    });
+
+  return run;
+});

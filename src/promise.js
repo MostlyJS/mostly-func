a37e47931a0c192a56cf87179fbf14be654fda4c
forafish <fora.luo@gmail.com>
2018-04-16 15:40:56 +0800

    add allP/delay

--- src/promise.js
+++ src/promise.js
@@ -7,4 +7,14 @@ import R from 'ramda';
  */
 export const tapP = promiseFn => arg => promiseFn(arg).then(R.always(arg));
 
+// :: ERROR_CODE -> Error -> Promise.Reject Error
+export const rejectWithCode = R.curry((code, err) =>
+  Promise.reject(Object.assign(err, { errorCode: code }))
+);
+
+// :: [Promise a] -> Promise a
+export const allP = promises => Promise.all(promises);
+
+// :: Number -> Promise.Resolved ()
+export const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 

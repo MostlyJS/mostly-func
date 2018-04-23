afba5762a6e8df0bdb5e3ab4582d9f2fa2e52c52
forafish <fora.luo@gmail.com>
2018-04-24 02:51:37 +0800

    add capitalize

--- src/string.js
+++ src/string.js
@@ -5,6 +5,7 @@ import R from 'ramda';
  */
 export const splitHead = R.compose(R.head, R.split('.'));
 export const splitTail = R.compose(R.join('.'), R.tail, R.split('.'));
+export const capitalize = R.replace(/^./, R.toUpper);
 
 /**
  * Always get an array of a possible input.

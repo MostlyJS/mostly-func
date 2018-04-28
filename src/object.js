971a1ee2de9e3808c380f7c5c30dafa0baf4886e
forafish <fora.luo@gmail.com>
2018-04-28 17:53:46 +0800

    rename mapObj to mapKV

--- src/object.js
+++ src/object.js
@@ -170,11 +170,11 @@ export const mapKeys = R.curry((fn, obj) =>
 /**
  * Map keys/values of an object
  *
- * usage: mapKeysAndValues(([a, b]) => [b, a], { foo: "bar", baz: "boo" })
+ * usage: mapKV(([a, b]) => [b, a], { foo: "bar", baz: "boo" })
  *        // { bar: "foo", boo: "baz" }
  */
 // :: ([a] -> [b]) -> Object -> Object
-export const mapObj = R.useWith(R.compose(R.fromPairs, R.map), [R.identity, R.toPairs]);
+export const mapKV = R.useWith(R.compose(R.fromPairs, R.map), [R.identity, R.toPairs]);
 
 /**
  * Make an object from an array using a mapper function

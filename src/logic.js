056d704120cffcc3164dd216d031610f73947859
forafish <fora.luo@gmail.com>
2018-07-10 18:53:20 +0800

    convert logic to cjs

--- src/logic.js
+++ src/logic.js
@@ -1,17 +1,4 @@
-import R from 'ramda';
-
-/**
- * In JavaScript, a `truthy` value is a value that is considered true
- * when evaluated in a Boolean context. All values are truthy unless
- * they are defined as falsy (i.e., except for `false`, `0`, `""`, `null`, `undefined`, and `NaN`).
- */
-export const isTruthy = R.pipe(Boolean, R.equals(true));
-
-/**
- * A falsy value is a value that translates to false when evaluated in a Boolean context.
- * Falsy values are `false`, `0`, `""`, `null`, `undefined`, and `NaN`.
- */
-export const isFalsy = R.complement(isTruthy);
+const R = require('ramda');
 
 /**
  * Count of values satifies a provided function
@@ -19,5 +6,23 @@ export const isFalsy = R.complement(isTruthy);
  * usage: countIf(isTruthy, [null, 1, 0, true]) // 2
  */
 // (a -> Boolean) -> [a] -> Number
-export const countIf = R.curry(R.compose(R.length, R.filter));
+const countIf = R.curry(R.compose(R.length, R.filter));
 
+/**
+ * In JavaScript, a `truthy` value is a value that is considered true
+ * when evaluated in a Boolean context. All values are truthy unless
+ * they are defined as falsy (i.e., except for `false`, `0`, `""`, `null`, `undefined`, and `NaN`).
+ */
+const isTruthy = R.pipe(Boolean, R.equals(true));
+
+/**
+ * A falsy value is a value that translates to false when evaluated in a Boolean context.
+ * Falsy values are `false`, `0`, `""`, `null`, `undefined`, and `NaN`.
+ */
+const isFalsy = R.complement(isTruthy);
+
+module.exports = {
+  countIf,
+  isFalsy,
+  isTruthy
+};

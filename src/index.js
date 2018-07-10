6484d2b93a3bf6ca053b45e2b2bb2a5924058324
forafish <fora.luo@gmail.com>
2018-07-10 18:59:38 +0800

    covert to cjs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import ramda from 'ramda';
-import * as sanctuary from './sanctuary';
-import * as func from './function';
-import * as future from './future';
-import * as list from './list';
-import * as logic from './logic';
-import * as math from './math';
-import * as monads from './monads';
-import * as object from './object';
-import * as promise from './promise';
-import * as relation from './relation';
-import * as string from './string';
-import * as type from './type';
+const ramda = require('ramda');
+const func = require('./function');
+const future = require('./future');
+const list = require('./list');
+const logic = require('./logic');
+const math = require('./math');
+const monads = require('./monads');
+const object = require('./object');
+const promise = require('./promise');
+const relation = require('./relation');
+const string = require('./string');
+const type = require('./type');
 
-export default Object.assign(ramda, sanctuary,
+module.exports = Object.assign(ramda,
   func, future, list, logic, math, monads, object, promise, relation, string, type
 );

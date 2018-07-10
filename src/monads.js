9d71be7437727440b6d49e67ebfab08a39a8dbe3
forafish <fora.luo@gmail.com>
2018-07-10 18:53:49 +0800

    convert monads to cjs

--- src/monads.js
+++ src/monads.js
@@ -1,11 +1,13 @@
-import R from 'ramda';
-import { default as S, Maybe, Either } from 'sanctuary';
+const R = require('ramda');
+const S = require('sanctuary');
+
+const { Maybe, Either } = S;
 
 /**
  * Adapter/Shortcust for eaiser interop
  */
-export const eitherLeft = Either.Left = S.Left;
-export const eitherRight = Either.Right = S.Right;
+const eitherLeft = Either.Left = S.Left;
+const eitherRight = Either.Right = S.Right;
 
 Either.of = Either.prototype.of = S.of(Either);
 Maybe.of = Maybe.prototype.of = S.of(Maybe);
@@ -14,26 +16,26 @@ Maybe.of = Maybe.prototype.of = S.of(Maybe);
  * adapter function to extract value from `Either` by providing a function
  * to handle the types of values contained in both `Left` and `Right`.
  */
-export const foldEither = S.either;
+const foldEither = S.either;
 
 /**
  * adapter function to extract value from `Maybe` by providing
  * a default value for `Nothing` and function to handle `Just`.
  */
-export const foldMaybe = S.maybe;
+const foldMaybe = S.maybe;
 
 /**
  * Function to rapidly extract a value from Maybe.Just
  * or throw a defined error message on Maybe.Nothing
  */
 // :: String -> Maybe a -> a
-export const explodeMaybe = (errorMessage, maybeObject) => {
+const explodeMaybe = (errorMessage, maybeObject) => {
   if (maybeObject.isJust) return maybeObject.value;
   throw new Error(errorMessage);
 };
 
 // :: [Maybe a] -> [a]
-export const catMaybies = (maybeObjects) => R.compose(
+const catMaybies = (maybeObjects) => R.compose(
   R.map(explodeMaybe('Expected Just, but got Nothing.')),
   R.filter(Maybe.isJust)
 )(maybeObjects);
@@ -43,18 +45,18 @@ export const catMaybies = (maybeObjects) => R.compose(
  * if it is Left.
  */
 // :: Either a b -> b
-export const explodeEither = foldEither(err => {
+const explodeEither = foldEither(err => {
   throw new Error(`Explosion failed: ${err}`);
 }, R.identity);
 
 // :: Either a b -> Promise a b
-export const eitherToPromise = foldEither(
+const eitherToPromise = foldEither(
   Promise.reject.bind(Promise),
   Promise.resolve.bind(Promise)
 );
 
 // :: (() -> b) -> (a -> a) -> Maybe a -> Promise a b
-export const maybeToPromise = R.curry((nothingFn, justFn, maybe) =>
+const maybeToPromise = R.curry((nothingFn, justFn, maybe) =>
   new Promise((resolve, reject) =>
     (maybe.isJust? resolve(maybe.value) : reject())
   ).then(justFn, nothingFn)
@@ -70,7 +72,7 @@ export const maybeToPromise = R.curry((nothingFn, justFn, maybe) =>
  * upsertLinks = reduceEither(assocLink, patch, [link0, link1, link2]);
  */
 // :: (b -> m b) -> (b -> a -> m b) -> b -> [a] -> m c
-export const reduceM = R.curry((m, fn, initial, list) =>
+const reduceM = R.curry((m, fn, initial, list) =>
   R.reduce((acc, a) => R.chain(val => fn(val, a), acc), m(initial), list)
 );
 
@@ -80,7 +82,7 @@ export const reduceM = R.curry((m, fn, initial, list) =>
  * @see reduceM for more details
  */
 // :: (b -> a -> Either c b) -> b -> [a] -> Either c b
-export const reduceEither = reduceM(Either.of);
+const reduceEither = reduceM(Either.of);
 
 /**
  * Returns a result of calling iterator function over argument list.
@@ -88,7 +90,7 @@ export const reduceEither = reduceM(Either.of);
  * @see reduceM for more details
  */
 // :: (b -> a -> Maybe b) -> b -> [a] -> Maybe b
-export const reduceMaybe = reduceM(Maybe.of);
+const reduceMaybe = reduceM(Maybe.of);
 
 /**
  * Returns Either b c.
@@ -106,6 +108,24 @@ export const reduceMaybe = reduceM(Maybe.of);
  * ```
  */
 // :: (a -> c) -> (a -> b) -> a -> Either b c
-export const leftIf = R.curry((condition, leftFn, val) =>
+const leftIf = R.curry((condition, leftFn, val) =>
     condition(val)? Either.of(val) : S.Left(leftFn(val))
 );
+
+module.exports = {
+  Maybe,
+  Either,
+  catMaybies,
+  eitherLeft,
+  eitherRight,
+  eitherToPromise,
+  explodeEither,
+  explodeMaybe,
+  foldEither,
+  foldMaybe,
+  leftIf,
+  maybeToPromise,
+  reduceEither,
+  reduceM,
+  reduceMaybe
+};
